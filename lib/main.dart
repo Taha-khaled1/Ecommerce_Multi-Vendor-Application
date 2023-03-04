879c979fa0764d12f40187980f27c20afca2320c
Taha khalid <tth31770@gmail.com>
2023-03-04 07:09:00 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pisti/application_layer/constant/myapp.dart';
 import 'package:pisti/data_layer/database/connectSqlflite.dart';
@@ -12,9 +10,6 @@ void main() async {
   sharedPreferences = await SharedPreferences.getInstance();
   sqlDb = SqlDb();
   runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const MyApp(), // Wrap your app
-    ),
+    const MyApp(), // Wrap your app
   );
 }
